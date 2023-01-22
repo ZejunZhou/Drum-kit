2cbf9f231b1a2e668d11350997e923cfb97183d9
ZejunZhou <zzhou443>
2023-01-22 20:48:56 +0800

    updating JS-Object Notes

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+//creating audio object
+var audio = new Audio("sounds/crash.mp3");
+
+var student = {
+    age:1,
+    class:2
+}
+
+
+
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-        alert("clicked");
+       this.style.color = "white";
+       audio.play();
+       console.log(this.innerHTML);
     })
 }
