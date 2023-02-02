aaad1d6d1e4726ef77568b0cc7ba806f1cc6032b
ZejunZhou <zzhou443>
2023-02-01 22:15:08 -0600

    refactor the code

--- index.js
+++ index.js
@@ -1,49 +1,9 @@
-//creating audio object
-
-
-var student = {
-    age:1,
-    class:2
-}
-
 /*********add even listner for all button */
 
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function(){
        var buttonHtml = this.innerHTML;
-       switch (buttonHtml) {
-        case "w":
-            var audio = new Audio("sounds/crash.mp3");
-            audio.play();
-            break;
-        case "a":
-            var audio = new Audio("sounds/kick-bass.mp3");
-            audio.play();
-            break;
-        case "s":
-            var audio = new Audio("sounds/snare.mp3");
-            audio.play();
-            break;
-        case "d":
-            var audio = new Audio("sounds/tom-1.mp3");
-            audio.play();
-            break;
-        case "j":
-            var audio = new Audio("sounds/tom-2.mp3");
-            audio.play();
-            break;
-        case "k":
-            var audio = new Audio("sounds/tom-3.mp3");
-            audio.play();
-            break;
-        case "l":
-            var audio = new Audio("sounds/tom-4.mp3");
-            audio.play();
-            break;
-        //print the case if reach the default
-        default:
-            console.log(buttonhtml);
-       }
+       createSound(buttonHtml);
     })
 }
 
@@ -51,8 +11,12 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++){
 
 document.addEventListener("keydown", function(keyDownEvent){
     var keyPressed = keyDownEvent.key;
+    createSound(keyPressed);
+})
 
-    switch(keyPressed){
+/****take sounds value of different event such as click and keydown */
+function createSound (sounds){
+    switch(sounds){
         case "w":
             var audio = new Audio("sounds/crash.mp3");
             audio.play();
@@ -83,6 +47,6 @@ document.addEventListener("keydown", function(keyDownEvent){
             break;
         //print the case if reach the default
         default:
-            console.log(keyPressed);
+            console.log(sounds);
     }
-})
+}
