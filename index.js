bdb9c10e8d4df02fb9d5b880366ccdf47acc0b45
ZejunZhou <zzhou443>
2023-02-01 22:09:10 -0600

    add event listener to keyboard

--- index.js
+++ index.js
@@ -1,17 +1,88 @@
 //creating audio object
-var audio = new Audio("sounds/crash.mp3");
+
 
 var student = {
     age:1,
     class:2
 }
 
-
+/*********add even listner for all button */
 
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-       this.style.color = "white";
-       audio.play();
-       console.log(this.innerHTML);
+       var buttonHtml = this.innerHTML;
+       switch (buttonHtml) {
+        case "w":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        //print the case if reach the default
+        default:
+            console.log(buttonhtml);
+       }
     })
 }
+
+/***********Add eventlister to target entire page, keydown as event and asynoumous function as response */
+
+document.addEventListener("keydown", function(keyDownEvent){
+    var keyPressed = keyDownEvent.key;
+
+    switch(keyPressed){
+        case "w":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        //print the case if reach the default
+        default:
+            console.log(keyPressed);
+    }
+})
